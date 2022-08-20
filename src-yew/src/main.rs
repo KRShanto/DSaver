493b6fc65efe17011fcb205714ee2ee3818976bb
KR Shanto <khalilorrahman2005@gmail.com>
2022-08-20 19:43:35 +0600

    create the struct `Link` to play with links

--- src-yew/src/main.rs
+++ src-yew/src/main.rs
@@ -1,4 +1,16 @@
+use serde::{Deserialize, Serialize};
 use yew::prelude::*;
+
+#[derive(Clone, Serialize, Deserialize, Debug, PartialEq)]
+struct Link {
+    url: String,       // manually
+    title: String,     // automatically
+    tags: Vec<String>, // manually  // filterable --- keep|remove
+    prirority: char,   // manually  // filterable --- A, B, C...|...C, B, A
+    browser: String,   // manually  // filterable --- keep|remove
+    complete: bool,    // manually  // filterable --- ture-false|false-true
+    date: String,      // automatically // filterable --- latest|oldest
+}
 
 fn main() {
     yew::start_app::<App>();
