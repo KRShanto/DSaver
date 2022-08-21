1e02bb3e4f2512b5856eeb3ef222ee39b5cd4b98
KR Shanto <khalilorrahman2005@gmail.com>
2022-08-21 22:11:27 +0600

    Move the `Link` type to a new crate `link-types`

---
+++ link-types/src/lib.rs
@@ -0,0 +1,17 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Serialize, Deserialize, Debug, PartialEq)]
+pub struct Link {
+    pub url: String,           // manually
+    pub title: Option<String>, // automatically // At the creation of the link, the title will be empty.  It will be filled at saving time. And shouldn't be None at get time.
+    pub tags: Vec<String>,     // manually  // filterable --- keep|remove
+    pub prirority: char,       // manually  // filterable --- A, B, C...|...C, B, A
+    pub browser: String,       // manually  // filterable --- keep|remove
+    pub complete: bool,        // manually  // filterable --- ture-false|false-true
+    pub date: String,          // automatically // filterable --- latest|oldest
+}
+
+#[derive(Clone, Serialize, Deserialize, Debug, PartialEq)]
+pub enum LinkSavingError {
+    WebpageNotFound,
+}
--- src-yew/src/main.rs
+++ src-yew/src/main.rs
@@ -1,15 +1,7 @@
 use serde::{Deserialize, Serialize};
+use link_types::{Link, LinkSavingError};
 use yew::prelude::*;
 
-#[derive(Clone, Serialize, Deserialize, Debug, PartialEq)]
-struct Link {
-    url: String,       // manually
-    title: String,     // automatically
-    tags: Vec<String>, // manually  // filterable --- keep|remove
-    prirority: char,   // manually  // filterable --- A, B, C...|...C, B, A
-    browser: String,   // manually  // filterable --- keep|remove
-    complete: bool,    // manually  // filterable --- ture-false|false-true
-    date: String,      // automatically // filterable --- latest|oldest
 }
 
 fn main() {
