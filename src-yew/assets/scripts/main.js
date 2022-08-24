1d8f9147d3cd619c9978bec915e81c14683604d1
KR Shanto <khalilorrahman2005@gmail.com>
2022-08-24 19:57:45 +0600

    Split the storing data functionality into two functions `addData` and `storeData`.

--- src-yew/assets/scripts/main.js
+++ src-yew/assets/scripts/main.js
@@ -13,26 +13,47 @@ export function isWebview() {
     return false;
 }
 
-// Store data in the file system or localstorage
-export async function storeData(fullDataArg, newData) {
+// Store data in the file system or localstorage. It will return Option<String>. If it returns None, it means success. Else it means error. You can use the error by parsing it from json to Rust structs.
+export async function storeData(fullData) {
     if (isWebview()) {
         // If the app runningin webview, store data in the file system
         const { writeTextFile, createDir, BaseDirectory } = window.__TAURI__.fs;
+
+        try {
+            // create the directory
+            await createDir(ROOT_DIR, { dir: BaseDirectory.Home, recursive: true });
+            // write the data into the file
+            await writeTextFile(`${ROOT_DIR}/links.json`, JSON.stringify(fullData), { dir: BaseDirectory.Home, recursive: true });
+
+            return null;
+        } catch (error) {
+            console.log("ERROR: ", error);
+            return JSON.stringify(error);
+        }
+    } else {
+        // If the app running in the browser, store data in the localstorage
+        return localStorage.setItem("data", data);
+    }
+}
+
+// Add data in the file system. It will return Option<String>. It will always return String. The string can be used by by parsing it from json to Rust structs. If this function successfully adds the data, then it will return `Link`. If any error occur, it will probably return `LinkSavingError`.
+export async function addData(fullDataArg, newData) {
+    if (isWebview()) {
+        // If the app runningin webview, store data in the file system
         const invoke = window.__TAURI__.invoke;
 
+        // parse the json into a js array
         const fullData = JSON.parse(fullDataArg);
 
         try {
+            // Validate the link and get new link from it
             const returnedData = await invoke("validate_link", { link: newData });
             console.log("New returned data ", returnedData);
 
             // push the new data to the full data
             fullData.push(returnedData);
 
-            // create the directory
-            await createDir(ROOT_DIR, { dir: BaseDirectory.Home, recursive: true });
-            // write the data into the file
-            await writeTextFile(`${ROOT_DIR}/links.json`, JSON.stringify(fullData), { dir: BaseDirectory.Home, recursive: true });
+            await storeData(fullData);
 
             return JSON.stringify(returnedData);
         } catch (error) {
@@ -45,7 +66,7 @@ export async function storeData(fullDataArg, newData) {
     }
 }
 
-// Get data from the file system or localstorage
+// Get data from the file system or localstorage. It returns Option<String>. It will be None if the file is not exits or can't read the file. The string can be used by parsing it from json to Rust struct. You can parse the String to Vec<Link> if the file contains valid data
 export async function getData() {
     if (isWebview()) {
         // If the app runningin webview, get data from the file system
