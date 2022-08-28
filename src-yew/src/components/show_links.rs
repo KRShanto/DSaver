62229046f7ca3e0a8823f715aca34974b52fd85e
KR Shanto <khalilorrahman2005@gmail.com>
2022-08-28 06:13:34 +0600

    Display links by filter their priority

--- src-yew/src/components/show_links.rs
+++ src-yew/src/components/show_links.rs
@@ -38,63 +38,86 @@ pub fn show_links() -> Html {
 
     let displayed_links = displayed_links_for_browsers.into_iter().unique();
 
+    let mut prirorities = Vec::new();
+
+    for link in &*links {
+        prirorities.push(link.prirority);
+    }
+
+    prirorities.sort();
+    prirorities.dedup();
+
     html! {
         <>
         <div>
             {
-            displayed_links.into_iter().map(|link| {
+            prirorities.iter().map(|prirority| {
                 html! {
                     <>
-                    <br />
-                    <p>{"URL: "}{&link.url}</p>
-                    <p>{format!("Title: {:?}", link.title)}</p>
-                    <p>{"Tags: "}</p>
-                    <u>
-                        {
-                            link.tags.iter().map(|tag| {
-                                html! {
-                                    <li>{tag}</li>
-                                }
-                            }).collect::<Html>()
+                    <h1>{prirority}</h1>
+
+                    {
+                    displayed_links.clone().into_iter().map(|link| {
+                        if &link.prirority == prirority {
+                            html! {
+                                <>
+                                <br />
+                                <p>{"URL: "}{&link.url}</p>
+                                <p>{format!("Title: {:?}", link.title)}</p>
+                                <p>{"Tags: "}</p>
+                                <u>
+                                {
+                                    link.tags.iter().map(|tag| {
+                                        html! {
+                                                <li>{tag}</li>
+                                            }
+                                        }).collect::<Html>()
+                                    }
+                                    </u>
+                                <p>{"Priority: "}{link.prirority}</p>
+                                <p>{"Browser: "}{&link.browser}</p>
+                                <p>{"Complete: "}{link.complete}</p>
+                                <p>{"Date: "}{&link.date}</p>
+                                <button onclick={
+                                    let edit_link_state = edit_link_state.clone();
+                                    let editing_link_id = editing_link_id.clone();
+                                    move |_| {
+                                        editing_link_id.set(Some(link.id));
+                                        edit_link_state.set(true);
+                                    }
+                                }>{"Edit"}</button>
+                                <button onclick={
+                                    let links = links.clone();
+                                    move |_| {
+                                        let mut old_links = (*links).clone();
+
+                                        // remove this link from `old_links`
+                                        old_links.retain(|old_link| old_link != &link);
+
+                                        links.set(old_links.clone());
+
+                                        // store the links to the filesystem
+                                        spawn_local(async move {
+                                            let result = store_data(struct_to_string(&old_links).unwrap())
+                                            .await
+                                            .unwrap();
+
+                                            // if the result is null, it means success
+                                            if let Some(error) = result.as_string() {
+                                                console_error!(error);
+                                            } else {
+                                                console_log!("Successfully deleted");
+                                            }
+                                        });
+                                    }
+                                }>{"Delete"}</button>
+                                </>
+                            }
+                        } else {
+                            html!("")
                         }
-                    </u>
-                    <p>{"Priority: "}{link.prirority}</p>
-                    <p>{"Browser: "}{&link.browser}</p>
-                    <p>{"Complete: "}{link.complete}</p>
-                    <p>{"Date: "}{&link.date}</p>
-                    <button onclick={
-                        let edit_link_state = edit_link_state.clone();
-                        let editing_link_id = editing_link_id.clone();
-                        move |_| {
-                            editing_link_id.set(Some(link.id));
-                            edit_link_state.set(true);
-                        }
-                    }>{"Edit"}</button>
-                    <button onclick={
-                        let links = links.clone();
-                        move |_| {
-                            let mut old_links = (*links).clone();
-
-                            // remove this link from `old_links`
-                            old_links.retain(|old_link| old_link != &link);
-
-                            links.set(old_links.clone());
-
-                            // store the links to the filesystem
-                            spawn_local(async move {
-                                let result = store_data(struct_to_string(&old_links).unwrap())
-                                    .await
-                                    .unwrap();
-
-                                // if the result is null, it means success
-                                if let Some(error) = result.as_string() {
-                                    console_error!(error);
-                                } else {
-                                    console_log!("Successfully deleted");
-                                }
-                            });
-                        }
-                    }>{"Delete"}</button>
+                    }).collect::<Html>()
+                    }
                     </>
                 }
             }).collect::<Html>()
