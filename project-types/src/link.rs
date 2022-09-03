6b4cfa61b6d2fba64054c2e44325aebbdf4dfefa
KR Shanto <khalilorrahman2005@gmail.com>
2022-09-03 13:28:27 +0600

    Create methods for `Link` for creating the struct using the builder pattern

--- project-types/src/link.rs
+++ project-types/src/link.rs
@@ -1,4 +1,6 @@
 use crate::Browser;
+#[cfg(feature = "wasm")]
+use js_sys::Date;
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
@@ -13,6 +15,106 @@ pub struct Link {
     pub browser: Browser,       // manually  // filterable --- keep|remove
     pub complete: bool,         // manually  // filterable --- ture-false|false-true
     pub date: String,           // automatically // filterable --- latest|oldest
+}
+
+impl Link {
+    pub fn new(url: String) -> Self {
+        Link {
+            id: Uuid::new_v4(),
+            url,
+            title: None,
+            domain: None,
+            tags: Vec::new(),
+            priority: 'A',
+            browser: Browser::Default,
+            complete: false,
+            date: String::from(""),
+        }
+    }
+
+    #[cfg(feature = "wasm")]
+    pub fn new_with_date(url: String) -> Self {
+        let months = [
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December",
+        ];
+
+        let today = Date::new_0();
+
+        let date = format!(
+            "{date} {month} {year}",
+            date = today.get_date(),
+            month = months[today.get_month() as usize],
+            year = today.get_full_year()
+        );
+
+        Link {
+            id: Uuid::new_v4(),
+            url,
+            title: None,
+            domain: None,
+            tags: Vec::new(),
+            priority: 'A',
+            browser: Browser::Default,
+            complete: false,
+            date,
+        }
+    }
+
+    pub fn id(mut self, id: Uuid) -> Self {
+        self.id = id;
+        self
+    }
+
+    pub fn url(mut self, url: String) -> Self {
+        self.url = url;
+        self
+    }
+
+    pub fn title(mut self, title: Option<String>) -> Self {
+        self.title = title;
+        self
+    }
+
+    pub fn domain(mut self, domain: Option<String>) -> Self {
+        self.domain = domain;
+        self
+    }
+
+    pub fn tags(mut self, tags: Vec<String>) -> Self {
+        self.tags = tags;
+        self
+    }
+
+    pub fn priority(mut self, priority: char) -> Self {
+        self.priority = priority;
+        self
+    }
+
+    pub fn browser(mut self, browser: Browser) -> Self {
+        self.browser = browser;
+        self
+    }
+
+    pub fn complete(mut self, complete: bool) -> Self {
+        self.complete = complete;
+        self
+    }
+
+    pub fn date(mut self, date: String) -> Self {
+        self.date = date;
+        self
+    }
 }
 
 #[derive(Clone, Serialize, Deserialize, Debug, PartialEq, Eq)]
