c722b03f16ab21654985e80dcfa87242b2c17016
KR Shanto <khalilorrahman2005@gmail.com>
2022-09-05 07:36:56 +0600

    Remove all checks for whether the app is running in localhost or webview. Now it is intend to run on webview.

--- src-yew/assets/scripts/main.js
+++ src-yew/assets/scripts/main.js
@@ -5,106 +5,83 @@
 // The directory to store data from the app
 const ROOT_DIR = ".link-saver";
 
-// Returns is the app running in webview or in the browser
-export function isWebview() {
-    if (window.__TAURI__) {
-        return true;
-    }
-    return false;
-}
 
-// Store data in the file system or localstorage. It will return Option<String>. If it returns None, it means success. Else it means error. 
+// Store data in the file system. It will return Option<String>. If it returns None, it means success. Else it means error. 
 export async function storeData(fullData) {
-    if (isWebview()) {
-        // If the app runningin webview, store data in the file system
-        const { writeTextFile, createDir, BaseDirectory } = window.__TAURI__.fs;
+    const { writeTextFile, createDir, BaseDirectory } = window.__TAURI__.fs;
 
-        try {
-            // create the directory
-            await createDir(ROOT_DIR, { dir: BaseDirectory.Home, recursive: true });
-            // write the data into the file
-            await writeTextFile(`${ROOT_DIR}/links.json`, fullData, { dir: BaseDirectory.Home, recursive: true });
+    try {
+        // create the directory
+        await createDir(ROOT_DIR, { dir: BaseDirectory.Home, recursive: true });
+        // write the data into the file
+        await writeTextFile(`${ROOT_DIR}/links.json`, fullData, { dir: BaseDirectory.Home, recursive: true });
 
-            return null;
-        } catch (error) {
-            console.log("ERROR: ", error);
-            return JSON.stringify(error);
-        }
-    } else {
-        // If the app running in the browser, store data in the localstorage
-        return localStorage.setItem("data", data);
+        return null;
+    } catch (error) {
+        console.log("ERROR: ", error);
+        return JSON.stringify(error);
     }
+
 }
 
-// Add data in the file system. It will return Option<String>. It will always return String. The string can be used by by parsing it from json to Rust structs. If this function successfully adds the data, then it will return `Link`. If any error occur, it will probably return `LinkSavingError`.
+// Add data in the file system. It will return Option<String>. It will always return String. The string can be used by by parsing it from json to Rust structs. If this function successfully adds the data, then it will return `Link`. If any error occur, it will probably return `ErrorReporter`.
 export async function addData(fullDataArg, newData) {
-    if (isWebview()) {
-        // If the app runningin webview, store data in the file system
-        const invoke = window.__TAURI__.invoke;
+    const invoke = window.__TAURI__.invoke;
+    // parse the json into a js array
+    const fullData = JSON.parse(fullDataArg);
 
-        // parse the json into a js array
-        const fullData = JSON.parse(fullDataArg);
+    try {
+        // Validate the link and get new link from it
+        const returnedData = await invoke("validate_link", { link: newData });
+        console.log("New returned data ", returnedData);
 
-        try {
-            // Validate the link and get new link from it
-            const returnedData = await invoke("validate_link", { link: newData });
-            console.log("New returned data ", returnedData);
+        // push the new data to the full data
+        fullData.push(returnedData);
 
-            // push the new data to the full data
-            fullData.push(returnedData);
+        await storeData(JSON.stringify(fullData));
 
-            await storeData(JSON.stringify(fullData));
-
-            return JSON.stringify(returnedData);
-        } catch (error) {
-            console.log("ERROR: ", error);
-            return JSON.stringify(error);
-        }
-    } else {
-        // If the app running in the browser, store data in the localstorage
-        return localStorage.setItem("data", data);
+        return JSON.stringify(returnedData);
+    } catch (error) {
+        console.log("ERROR: ", error);
+        return JSON.stringify(error);
     }
+
 }
 
-// Get data from the file system or localstorage. It returns Option<String>. It will be None if the file is not exits or can't read the file. The string can be used by parsing it from json to Rust struct. You can parse the String to Vec<Link> if the file contains valid data
+// Get data from the file system. It returns Option<String>. It will be None if the file is not exits or can't read the file. The string can be used by parsing it from json to Rust struct. You can parse the String to Vec<Link> if the file contains valid data
 export async function getData() {
-    if (isWebview()) {
-        // If the app runningin webview, get data from the file system
-        const { readTextFile, BaseDirectory } = window.__TAURI__.fs;
+    const { readTextFile, BaseDirectory } = window.__TAURI__.fs;
 
-        // The file may not exist, so we need to handle the error
-        try {
-            // if the file exists, read the data from the file
-            return await readTextFile(`${ROOT_DIR}/links.json`, { dir: BaseDirectory.Home });
-        } catch (error) {
-            // else return null
-            return null;
-        }
-    } else {
-        // If the app running in the browser, get data from the localstorage
-        return localStorage.getItem("data");
+    // The file may not exist, so we need to handle the error
+    try {
+        // if the file exists, read the data from the file
+        return await readTextFile(`${ROOT_DIR}/links.json`, { dir: BaseDirectory.Home });
+    } catch (error) {
+        // else return null
+        return null;
     }
+
 }
 
+// Open the browser corresponding to the the user's OS
 export async function openBrowser(path, browser) {
-    if (isWebview()) {
-        const invoke = window.__TAURI__.invoke;
-        const { platform } = window.__TAURI__.os;
+    const invoke = window.__TAURI__.invoke;
+    const { platform } = window.__TAURI__.os;
 
-        const platformName = await platform();
-        let functionName;
+    const platformName = await platform();
+    let functionName;
 
-        if (platformName === "win32") {
-            functionName = "open_browser_windows";
-        } else if (platformName === "linux") {
-            functionName = "open_browser_linux";
-        } else {
-            functionName = "open_browser_macos";
-        }
-
-        const result = await invoke(functionName, { path, browser });
-
-        return JSON.stringify(result);
-
+    if (platformName === "win32") {
+        functionName = "open_browser_windows";
+    } else if (platformName === "linux") {
+        functionName = "open_browser_linux";
+    } else {
+        functionName = "open_browser_macos";
     }
+
+    const result = await invoke(functionName, { path, browser });
+
+    return JSON.stringify(result);
+
+
 }
