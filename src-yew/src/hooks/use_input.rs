89df48173d65dc2482a199e2295d9704215454dc
KR Shanto <khalilorrahman2005@gmail.com>
2022-09-12 00:16:53 +0600

    Make some structs for future

---
+++ src-yew/src/hooks/use_input.rs
@@ -0,0 +1,26 @@
+use wasm_bindgen::JsCast;
+use web_sys::HtmlInputElement;
+use yew::prelude::*;
+
+// TODO:give example
+pub fn use_input(default: &str) -> (UseStateHandle<String>, Callback<KeyboardEvent>) {
+    let input_state = use_state(|| default.to_string());
+
+    let onkeyup = Callback::from({
+        let input_state = input_state.clone();
+
+        move |event: KeyboardEvent| {
+            let event = event.target().unwrap();
+            let value = event.dyn_into::<HtmlInputElement>().unwrap().value();
+
+            input_state.set(value);
+        }
+    });
+
+    (input_state, onkeyup)
+}
+
+// FEATURE: In future
+pub struct InputOptions {
+    disable: bool,
+}
